0928f7c6ccff251e958db48ed0668e0b657a141d
agustik <agust.ingi.kjartansson@gmail.com>
2014-05-13 16:07:11 +0000

    first js

--- js/razor.js
+++ js/razor.js
@@ -1,1 +1,14 @@
-razor.js
+
+$(document).ready(function (){
+	GetNodes();
+});
+
+function GetNodes(){
+	$.ajax({
+		url : config.server + '/collections/nodes';
+		dataType:'json',
+		success: function (d){
+			console.log(d);
+		}
+	});
+}
