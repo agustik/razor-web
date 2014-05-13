ae9a0739cc33f0fba0705ef1a425f96624de9f6d
agustik <agust.ingi.kjartansson@gmail.com>
2014-05-13 17:32:59 +0000

    .

--- js/razor.js
+++ js/razor.js
@@ -1,28 +1,47 @@
 
-$(document).ready(function (){
-	GetNodes();
-});
-
 
 var razor = {
-	nodes : function (nodes){
-		var tr;
-		$.each(nodes, function (nr, node){
-			tr = "<td>"+node.name+"<td>";
-			$('#nodes').append(tr);
+	init : function (){
+		console.log('init');
+		razor.x = this;
+	},
+	get : function (api, callback){
+		$.ajax({
+			url : config.server + api,
+			dataType:'json'
+		}).done(function (d){
+			callback(d);
 		});
-	}
-}
+	},
 
-
-
-function GetNodes(){
-	$.ajax({
-		url : config.server + '/collections/nodes',
-		dataType:'json',
-		success: function (d){
-			razor.nodes(d.items);
+	nodes : function (callback){
+		var r = this;
+			this.get('/collections/nodes', function (d){
+				$.each(d.items, function (nr, node){
+					r.get('/collections/nodes/'+node.name, function (d){
+						callback(d);
+					});
+				});
+			});
 		}
-	});
+
 }
 
+
+
+$(document).ready(function (){
+	razor.init();
+
+	razor.nodes(function (d){
+		console.log(d);
+		var tr = "<tr>";
+			tr +="<td>"+d.name+"</td>";
+			tr +="<td>"+d.facts.macaddress+"</td>";
+			tr +="<td>"+d.facts.physicalprocessorcount+"</td>";
+			tr +="<td>"+d.facts.memorysize_mb+"</td>";
+			tr +="<td>"+d.facts.blockdevice_sda_size+"</td>";
+			tr +="<td>"+d.last_checkin+"</td>";
+			tr +="</tr>";
+		$('#nodes tbody').append(tr);
+	});
+});
